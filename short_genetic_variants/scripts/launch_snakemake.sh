a120d0e7bf3e62b7c9840c506193b67c5711bd19
sam per <prnsml@gmail.com>
2019-09-09 11:27:52 +0200

    30G and 3cores

--- short_genetic_variants/scripts/launch_snakemake.sh
+++ short_genetic_variants/scripts/launch_snakemake.sh
@@ -2,7 +2,7 @@
 
 # request memory for job (default 6G, max 72G)
 #$ -pe smp 3
-#$ -l rmem=20G
+#$ -l rmem=10G
 # run time for job in hours:mins:sec (max 168:0:0, jobs with h_rt < 8:0:0 have priority)
 #$ -l h_rt=00:59:59
 #$ -cwd
