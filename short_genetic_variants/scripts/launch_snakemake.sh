b6f20d7ca3ca86296b9225bef1fad7a7c82fca56
sam per <prnsml@gmail.com>
2019-10-31 11:41:13 +0100

    more mem for all target regions gatk import

--- short_genetic_variants/scripts/launch_snakemake.sh
+++ short_genetic_variants/scripts/launch_snakemake.sh
@@ -2,7 +2,7 @@
 
 # request memory for job (default 6G, max 72G)
 #$ -pe smp 4
-#$ -l rmem=25G
+#$ -l rmem=29G
 # run time for job in hours:mins:sec (max 168:0:0, jobs with h_rt < 8:0:0 have priority)
 #$ -l h_rt=94:59:59
 #$ -cwd
