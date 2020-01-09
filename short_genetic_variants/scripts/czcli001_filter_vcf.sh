755495608d0ef1b2932251aa629f0649746392ea
sam per <prnsml@gmail.com>
2020-01-09 10:09:20 +0100

    Anja filter vcf scr

---
+++ short_genetic_variants/scripts/czcli001_filter_vcf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ve
+
+# request memory for job (default 6G, max 72G)
+#$ -l mem=6G
+#$ -l rmem=6G
+# run time for job in hours:mins:sec (max 168:0:0, jobs with h_rt < 8:0:0 have priority)
+#$ -l h_rt=01:59:00
+#$ -P molecol
+#$ -q molecol.q
+#$ -t 1000-1967
+#$ -tc 30
+
+taskid=${SGE_TASK_ID}
+
+for site in CZA CZB CZD;do
+
+# make one file per location; keep only SNPs used in ANG analysis
+# /home/bo1awx/programs/vcftools_0.1.13/bin/vcftools --keep individuals_$site.txt --vcf CZ.raw.vcf$taskid \
+# --positions ANG13b_positions_to_keep.txt --recode --out CZCLI01_$site.filt1-$taskid
+
+# minor allele freq filter, variant quality filter, keep biallelic SNPs only, remove SNPs where < 150 inds have data
+/home/bo1awx/programs/vcftools_0.1.13/bin/vcftools --vcf CZCLI01_$site.filt1-$taskid.recode.vcf --maf 0.01 --minQ 20 \
+--min-alleles 2 --max-alleles 2 --max-missing-count 150 --recode --out CZCLI01_$site.filt2-$taskid
+
+done
+
