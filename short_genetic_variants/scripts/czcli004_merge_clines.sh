453ba5d03c1953054288b8b3a416b9a1e86f91d0
sam per <prnsml@gmail.com>
2020-01-27 08:47:24 +0100

    new path anja merge clines

--- short_genetic_variants/scripts/czcli004_merge_clines.sh
+++ short_genetic_variants/scripts/czcli004_merge_clines.sh
@@ -7,25 +7,26 @@
 #$ -l h_rt=06:00:00
 # current environment settings are used for the job???
 #$ -V
+#$ -cwd
 
-head -n 1 CZCLI003_cline_snps_CZA_left_1000.txt > CZCLI004_CZA_left_clines.txt
-cat CZCLI003_cline_snps_CZA_left_1*.txt | grep -v Index  >> CZCLI004_CZA_left_clines.txt
+head -n 1 Anja/czcli003_clines/CZCLI003_cline_snps_CZA_left_1000.txt > CZCLI004_CZA_left_clines.txt
+cat Anja/czcli003_clines/CZCLI003_cline_snps_CZA_left_1*.txt | grep -v Index  >> CZCLI004_CZA_left_clines.txt
 
-head -n 1 CZCLI003_cline_snps_CZA_right_1000.txt > CZCLI004_CZA_right_clines.txt
-cat CZCLI003_cline_snps_CZA_right_1*.txt | grep -v Index >> CZCLI004_CZA_right_clines.txt
-
-
-
-head -n 1 CZCLI003_cline_snps_CZB_left_1000.txt > CZCLI004_CZB_left_clines.txt
-cat CZCLI003_cline_snps_CZB_left_1*.txt | grep -v Index >> CZCLI004_CZB_left_clines.txt
-
-head -n 1 CZCLI003_cline_snps_CZB_right_1000.txt > CZCLI004_CZB_right_clines.txt
-cat CZCLI003_cline_snps_CZB_right_1*.txt | grep -v Index >> CZCLI004_CZB_right_clines.txt
+head -n 1 Anja/czcli003_clines/CZCLI003_cline_snps_CZA_right_1000.txt > CZCLI004_CZA_right_clines.txt
+cat Anja/czcli003_clines/CZCLI003_cline_snps_CZA_right_1*.txt | grep -v Index >> CZCLI004_CZA_right_clines.txt
 
 
 
-head -n 1 CZCLI003_cline_snps_CZD_left_1000.txt > CZCLI004_CZD_left_clines.txt
-cat CZCLI003_cline_snps_CZD_left_1*.txt | grep -v Index >> CZCLI004_CZD_left_clines.txt
+head -n 1 Anja/czcli003_clines/CZCLI003_cline_snps_CZB_left_1000.txt > CZCLI004_CZB_left_clines.txt
+cat Anja/czcli003_clines/CZCLI003_cline_snps_CZB_left_1*.txt | grep -v Index >> CZCLI004_CZB_left_clines.txt
 
-head -n 1 CZCLI003_cline_snps_CZD_right_1000.txt > CZCLI004_CZD_right_clines.txt
-cat CZCLI003_cline_snps_CZD_right_1*.txt | grep -v Index >> CZCLI004_CZD_right_clines.txt
+head -n 1 Anja/czcli003_clines/CZCLI003_cline_snps_CZB_right_1000.txt > CZCLI004_CZB_right_clines.txt
+cat Anja/czcli003_clines/CZCLI003_cline_snps_CZB_right_1*.txt | grep -v Index >> CZCLI004_CZB_right_clines.txt
+
+
+
+head -n 1 Anja/czcli003_clines/CZCLI003_cline_snps_CZD_left_1000.txt > CZCLI004_CZD_left_clines.txt
+cat Anja/czcli003_clines/CZCLI003_cline_snps_CZD_left_1*.txt | grep -v Index >> CZCLI004_CZD_left_clines.txt
+
+head -n 1 Anja/czcli003_clines/CZCLI003_cline_snps_CZD_right_1000.txt > CZCLI004_CZD_right_clines.txt
+cat Anja/czcli003_clines/CZCLI003_cline_snps_CZD_right_1*.txt | grep -v Index >> CZCLI004_CZD_right_clines.txt
