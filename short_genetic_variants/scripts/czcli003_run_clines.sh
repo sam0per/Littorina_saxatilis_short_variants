aed321869c2c1b9b9326225359621cca9463795c
sam per <prnsml@gmail.com>
2020-06-01 08:53:43 +0200

    bash run cline.R indel

--- short_genetic_variants/scripts/czcli003_run_clines.sh
+++ short_genetic_variants/scripts/czcli003_run_clines.sh
@@ -6,22 +6,23 @@
 #$ -l mem=6G
 #$ -l rmem=6G
 # run time for job in hours:mins:sec (max 168:0:0, jobs with h_rt < 8:0:0 have priority)
-#$ -l h_rt=02:59:00
+#$ -l h_rt=02:50:00
 # current environment settings are used for the job
 #$ -V
 # -P molecol
 # -q molecol.q
-#$ -t 1000-1965
+#$ -t 1000-16828
 #$ -tc 30
 #$ -cwd
 
 taskid=${SGE_TASK_ID}
+vtype=INDEL
 
 module add apps/R/3.5.1
 
 for site in CZA CZB CZD; do
   for zone in left right; do
     Rscript --vanilla /home/bo4spe/Littorina_saxatilis/short_genetic_variants/scripts/czcli003_clines_20190725.R \
-    czcli002_allele_count/CZCLI02_${site}_SNP-${taskid}.alleles ${site}_${zone} ${taskid}
+    czcli002_allele_count/CZCLI02_${site}_${vtype}-${taskid}.alleles ${site}_${zone} ${taskid} ${vtype}
   done
 done
