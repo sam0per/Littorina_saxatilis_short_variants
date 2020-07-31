c7c802ee21156851f97bd24f43e275c66010a58c
sam per <prnsml@gmail.com>
2020-07-31 08:43:04 +0200

    summary freq wave left and right

--- short_genetic_variants/scripts/merge_ecotypes_allele_freq.sh
+++ short_genetic_variants/scripts/merge_ecotypes_allele_freq.sh
@@ -4,13 +4,13 @@
 #$ -l mem=6G
 #$ -l rmem=6G
 # run time for job in hours:mins:sec (max 168:0:0, jobs with h_rt < 8:0:0 have priority)
-#$ -l h_rt=01:50:00
+#$ -l h_rt=02:50:00
 #$ -cwd
 #$ -V
 
 for site in CZA CZB CZD; do
   for type in INDEL SNP; do
-    for ecot in CRAB WAVE; do
+    for ecot in CRAB WAVE_LEFT WAVE_RIGHT; do
 
     (head -1 allele_freq/${ecot}/AF_${site}_${ecot}_${type}.filt2-1000.frq && tail -n +2 -q allele_freq/${ecot}/AF_${site}_${ecot}_${type}.filt2-*.frq) > summary/allele_freq/${ecot}/AF_${site}_${ecot}_${type}.filt2.txt
 
