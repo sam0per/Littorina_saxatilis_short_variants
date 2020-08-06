3b592698a68ca49eb365dcca5d8970c0a39be62a
sam per <prnsml@gmail.com>
2020-08-06 17:27:37 +0200

    filter by n individuals in each ecotype

--- short_genetic_variants/scripts/ecotypes_allele_freq.sh
+++ short_genetic_variants/scripts/ecotypes_allele_freq.sh
@@ -16,14 +16,29 @@ source activate short-variants
 
 
 taskid=${SGE_TASK_ID}
+cutoff=10
 
 for site in CZA CZB CZD; do
   for type in INDEL SNP; do
     for ecot in CRAB WAVE_LEFT WAVE_RIGHT; do
 
-    vcftools --keep /home/bo4spe/Littorina_saxatilis/short_genetic_variants/${ecot}_${site}.txt \
-    --vcf filtered/CZCLI01_${site}_${type}.filt2-${taskid}.recode.vcf --freq \
-    --out allele_freq/${ecot}/AF_${site}_${ecot}_${type}.filt2-${taskid} || continue
+      touch out_rm_${site}_${ecot}_${type}.txt
+      touch out_in_${site}_${ecot}_${type}.txt
+
+      vcftools --keep /home/bo4spe/Littorina_saxatilis/short_genetic_variants/${ecot}_${site}.txt \
+      --vcf filtered/CZCLI01_${site}_${type}.filt2-${taskid}.recode.vcf --recode \
+      --out filtered/${ecot}/CZCLI01_${site}_${ecot}_${type}.filt2-${taskid} || continue
+
+      nindv=$(grep "#CHROM" filtered/${ecot}/CZCLI01_${site}_${ecot}_${type}.filt2-${taskid}.recode.vcf | tr "\t" "\n" | tail -n +10 | uniq | wc -l)
+      if [ "$nindv" -lt "$cutoff" ]
+      then
+        echo removed filtered/${ecot}/CZCLI01_${site}_${ecot}_${type}.filt2-${taskid}.recode.vcf because individuals = $nindv >> out_rm_${site}_${ecot}_${type}.txt
+        rm filtered/${ecot}/CZCLI01_${site}_${ecot}_${type}.filt2-${taskid}.recode.vcf
+      else
+        echo kept filtered/${ecot}/CZCLI01_${site}_${ecot}_${type}.filt2-${taskid}.recode.vcf because individuals = $nindv >> out_in_${site}_${ecot}_${type}.txt
+        vcftools --vcf filtered/${ecot}/CZCLI01_${site}_${ecot}_${type}.filt2-${taskid}.recode.vcf --freq \
+        --out allele_freq/${ecot}/AF_${site}_${ecot}_${type}.filt2-${taskid} || continue
+      fi
 
     done
   done
