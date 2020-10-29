37d486beb40e69fb7b4b907c66ad4d7848958ac1
sam p <prnsml@gmail.com>
2020-10-29 08:33:48 +0100

    test on D, H and SFS

--- short_genetic_variants/scripts/DH_patterns.R
+++ short_genetic_variants/scripts/DH_patterns.R
@@ -4,14 +4,15 @@ dh_res <- read.csv('results/DH_estimates.csv')
 head(dh_res)
 str(dh_res)
 dh_res$ECOT <- factor(dh_res$ECOT, levels = c("WAVE_LEFT", "CRAB", "WAVE_RIGHT"))
-dh_res$Variant_type <- factor(dh_res$Variant_type, levels = c("INDEL", "DELETION", "INSERTION", "SNP"))
+dh_res$Variant_type <- factor(dh_res$Variant_type, levels = c("INDEL", "DELETION", "INSERTION", "SNP", "WWSS"))
 levels(dh_res$ANN)
 dh_res$ANN <- factor(dh_res$ANN, levels = c("all", "noncoding", "nongenic", "coding", "inframe",
                                             "synonymous", "nonsynonymous", "frameshift",
                                             "WW", "SW", "WS", "SS"))
 
-vt <- c('INDEL', 'SNP')
+# vt <- c('INDEL', 'SNP')
 # vt <- c('SNP')
+vt <- c('DELETION', 'INSERTION', 'WWSS')
 
 library(RColorBrewer)
 # vpal <- c("#1B9E77", "#666666")
@@ -27,6 +28,7 @@ an <- c('nongenic', 'synonymous', 'nonsynonymous')
 # an <- 'coding'
 
 dh_sub <- dh_res[dh_res$Variant_type %in% vt & dh_res$ANN %in% an, ]
+table(dh_sub$Variant_type)
 dh_sub[dh_sub$ANN=='nongenic',]
 
 library(ggplot2)
@@ -79,8 +81,8 @@ lapply(X = an, FUN = function(x) {
   
   dh_sub <- dh_res[dh_res$Variant_type %in% vt & dh_res$ANN==x, ]
   
-  Dlm <- summary(lm(formula = D~Variant_type, data = dh_sub, weights = sqrt(segsites)))
-  Hlm <- summary(lm(formula = H~Variant_type, data = dh_sub, weights = sqrt(segsites)))
+  Dlm <- summary(lm(formula = D~-1 + Variant_type, data = dh_sub, weights = sqrt(segsites)))
+  Hlm <- summary(lm(formula = H~-1 + Variant_type, data = dh_sub, weights = sqrt(segsites)))
   
   return(list(x, Dlm, Hlm))
   
@@ -105,14 +107,17 @@ dh_vt <- rbind(data.frame(rbindlist(dh_vt[[1]])),
                data.frame(rbindlist(dh_vt[[2]])),
                data.frame(rbindlist(dh_vt[[3]])))
 str(dh_vt)
-dh_vt$Pal <- ifelse(test = dh_vt$Par == levels(dh_vt$Par)[1], yes = 'INDELs', no = 'SNPs')
+# dh_vt$Pal <- ifelse(test = dh_vt$Par == levels(dh_vt$Par)[1], yes = 'INDELs', no = 'SNPs')
+dh_vt$Pal <- substr(x = as.character(dh_vt$Par), start = 13, stop = nchar(as.character(dh_vt$Par)))
+
 dh_vt$Estimate <- as.numeric(as.character(dh_vt$Estimate))
 dh_vt$Std..Error <- as.numeric(as.character(dh_vt$Std..Error))
 btw <- ggplot(data = dh_vt, aes(x = ANN, y = Estimate, col = Pal)) +
   facet_grid(cols = vars(SS)) +
   geom_errorbar(aes(ymin = Estimate - Std..Error, ymax = Estimate + Std..Error), width=.1, size = 1) +
   geom_point(size = 4) +
-  scale_color_manual(values = c("#1B9E77", "#666666")) +
+  # scale_color_manual(values = c("#1B9E77", "#666666")) +
+  scale_color_manual(values = vpal) +
   labs(col = '', x = '', y = 'Fitted value') +
   theme(legend.text = element_text(size = 16),
         axis.text = element_text(size = 20),
@@ -131,7 +136,27 @@ ggsave(filename = 'figures/DH_between_variants.pdf', plot = btw, width = 8, heig
 # 
 # 
 # 
-rm(list = setdiff(ls(), c('dh_res', 'an', 'vt')))
+# rm(list = setdiff(ls(), c('dh_res', 'an', 'vt')))
+summary(lm(formula = D ~ Variant_type * ANN, data = dh_sub, weights = sqrt(segsites)))
+summary(lm(formula = D ~ -1 + Variant_type + ANN, data = dh_sub, weights = sqrt(segsites)))
+dh_one <- dh_sub[dh_sub$Variant_type=='WWSS', ]
+dh_one <- dh_one[dh_one$ECOT=='CRAB', ]
+dh_one <- split(x = dh_one, f = dh_one$ZONE)
+
+ggplot(data = dh_one, aes(x = ANN, y = D, col = ZONE)) +
+  geom_point(size = 3) +
+  labs(col = '', x = '') +
+  theme(axis.text = element_text(size = 18), legend.text = element_text(size = 16),
+        axis.title.y = element_text(size = 18))
+ggplot(data = dh_one, aes(x = ANN, y = H, col = ZONE)) +
+  geom_point(size = 3) +
+  labs(col = '', x = '') +
+  theme(axis.text = element_text(size = 18), legend.text = element_text(size = 16),
+        axis.title.y = element_text(size = 18))
+
+summary(lm(formula = D ~ -1 + ANN, data = dh_one$CZA, weights = sqrt(segsites)))
+summary(lm(formula = D ~ -1 + ANN, data = dh_one$CZB, weights = sqrt(segsites)))
+summary(lm(formula = D ~ -1 + ANN, data = dh_one$CZD, weights = sqrt(segsites)))
 # WITHIN INDELS AND SNPS
 lapply(X = vt, FUN = function(x) {
   
@@ -140,8 +165,8 @@ lapply(X = vt, FUN = function(x) {
   Dlm <- summary(lm(formula = D~-1 + ANN, data = dh_sub, weights = sqrt(segsites)))
   Hlm <- summary(lm(formula = H~-1 + ANN, data = dh_sub, weights = sqrt(segsites)))
   
-  # return(list(x, Dlm, Hlm))
-  return(list(coef(Dlm), coef(Hlm)))
+  return(list(x, Dlm, Hlm))
+  # return(list(coef(Dlm), coef(Hlm)))
   
 })
 # -0.14284723+0.05209618
