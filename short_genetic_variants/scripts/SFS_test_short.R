53ab30898c3d54796a0ef5b1ece9e4c9a487c851
sam per <prnsml@gmail.com>
2020-11-20 10:14:57 +0100

    new sfs comp with 2x2 table and chi-squared

--- short_genetic_variants/scripts/SFS_test_short.R
+++ short_genetic_variants/scripts/SFS_test_short.R
@@ -18,8 +18,8 @@ option_list = list(
   make_option(c("-t", "--types"), type = "character", default = NULL,
               help = "two types of comparison (e.g., INDEL:SNP, coding_INDEL:noncoding_INDEL or CRAB:WAVE).",
               metavar = "character"),
-  make_option(opt_str = "--rminv", action = 'store_true', default = FALSE,
-              help = "add this flag if variants inside inversions must be removed."))
+  make_option(opt_str = "--test1", action = 'store_true', default = FALSE,
+              help = "add this flag to perform Nielsen et al. 2005 Test1-like test."))
 
 opt_parser = OptionParser(usage = paste("Rscript scripts/SFS_test_short.R",
                                         "-i results/marker_density/MD_CZA_CRAB_coding_INDEL_count.txt",
@@ -64,11 +64,25 @@ da <- data.frame(N=rep(1:length(iid), 2), C=c(iid,jsd),
                  V=c(rep(tv[2], length(iid)), rep(tv[4], length(jsd))),
                  A=c(rep(tv[1], length(iid)), rep(tv[3], length(jsd))))
 da$AV <- paste(da$A, da$V)
+
+fac_pal <- as.vector(x = rep("NA", 2), mode = "character")
+if (grepl(pattern = "IN|DE", x = tv[2])) {
+  fac_pal[1] <- brewer.pal(n = 3, name = "Paired")[1]
+} else {
+  fac_pal[1] <- brewer.pal(n = 3, name = "Paired")[3]
+}
+
+if (grepl(pattern = "IN|DE", x = tv[4])) {
+  fac_pal[2] <- brewer.pal(n = 4, name = "Paired")[2]
+} else {
+  fac_pal[2] <- brewer.pal(n = 4, name = "Paired")[4]
+}
+
 DAC_h <- ggplot(data = da, aes(x = N, y = C)) +
   facet_wrap(facets = ~AV, nrow = 2, scales = 'free') +
   geom_col(aes(fill = AV), col = 'black', position = 'dodge') +
   # scale_fill_manual(values = c("#1B9E77", "#666666")) +
-  # scale_fill_manual(values = fac_pal) +
+  scale_fill_manual(values = fac_pal) +
   labs(x = '', y = 'Derived allele count') +
   theme(legend.position = 'none',
         axis.text = element_text(size = 11),
@@ -103,60 +117,125 @@ dDAP_h <- ggplot(data = data.frame(N=1:length(iid_p), dP=iid_p-jsd_p), aes(x = N
         panel.grid = element_line(colour = "gray70", size = 0.2))
 # dDAP_h
 
-class_contrib <- rep(0,((unique(sc$N)*2)-1))
-# TO AVOID INF AFTER LOG TRANSFORMATION
-jsd <- jsd + 1
-iid <- iid + 1
-jsd_p <- jsd/sum(jsd)
-iid_p <- iid/sum(iid)
-# alternative (better?) test version (testing whether indels and snps differ, rather than whether indels fit snp expectation)
+parts <- cnm[c(-1, (-nrow(cnm)+2):-nrow(cnm)), ]
+if (identical(as.character(parts[,1]),as.character(parts[,2]))) {
+  parts <- paste(as.character(parts[,1]), collapse = '_')
+} else {
+  parts <- paste(paste(as.character(parts[,1]), collapse = '_'), paste(as.character(parts[,2]), collapse = '_'), sep = '_')
+}
 
-joint_p <- (jsd+iid)/sum(jsd+iid)
+if (opt$test1) {
+  class_contrib <- rep(0,((unique(sc$N)*2)-1))
+  # TO AVOID INF AFTER LOG TRANSFORMATION
+  jsd <- jsd + 1
+  iid <- iid + 1
+  jsd_p <- jsd/sum(jsd)
+  iid_p <- iid/sum(iid)
+  # alternative (better?) test version (testing whether indels and snps differ, rather than whether indels fit snp expectation)
+  
+  joint_p <- (jsd+iid)/sum(jsd+iid)
+  
+  LL0 <- sum(iid*log(joint_p)) + sum(jsd*log(joint_p))
+  
+  LL1 <- sum(iid*log(iid_p)) + sum(jsd*log(jsd_p))
+  
+  a <- rep(0,(unique(sc$N)*2)-1)
+  for (i in 1:((unique(sc$N)*2)-1)){a[i] <- iid[i]*log(iid_p[i]) + jsd[i]*log(jsd_p[i]) + sum(iid[-i])*log(sum(iid_p[-i])) + sum(jsd[-i])*log(sum(jsd_p[-i]))}
+  b <- rep(0,(unique(sc$N)*2)-1)
+  for (i in 1:((unique(sc$N)*2)-1)){b[i] <- (iid[i]*log(joint_p[i])) + (jsd[i]*log(joint_p[i])) + sum(iid[-i])*log(sum(joint_p[-i])) + sum(jsd[-i])*log(sum(joint_p[-i]))}
+  
+  # class_contrib <- rep(0,((unique(sc$N)*2)-1))
+  for (i in 1:((unique(sc$N)*2)-1)){class_contrib[i] <- 2*(a[i]-b[i])}
+  
+  chi_app <- 2*(LL1-LL0)  # now with 16 df (I think)
+  cat('Chi-square test statistic =', chi_app, '\n')
+  # qchisq(p = 0.05, df = 260)
+  ndf <- ((unique(sc$N)*2)-1-1)*(length(unique(da$AV))-1)
+  cat('Chi-square critical value at 0.01 with', ndf, 'df =', qchisq(p = 0.01, df = ndf), '\n')
+  cat('Test statistic - critical value =', chi_app-qchisq(p = 0.01, df = ndf), '\n')
+  
+  chi_tb <- data.frame(chi2_stat=chi_app,
+                       chi2_crit=qchisq(p = 0.01, df = ndf),
+                       diff=chi_app-qchisq(p = 0.01, df = ndf))
+  # chi_tb
+  colnames(chi_tb) <- c("x2 stat", "x2 crit", "x2 stat-crit")
+  
+  mytheme.b <- gridExtra::ttheme_default(
+    core = list(fg_params=list(cex = 0.7)),
+    colhead = list(fg_params=list(cex = 0.7, col = 'black')),
+    rowhead = list(fg_params=list(cex = 0.7)))
+  contr_p <- ggplot(data = data.frame(N=1:((unique(ic$N)*2)-1), CC=class_contrib)) +
+    labs(x = 'Derived allele class', y = 'Contribution') +
+    theme(axis.text = element_text(size = 11),
+          axis.title = element_text(size = 14),
+          panel.background = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+          axis.line = element_line(size = 0.2, linetype = "solid",
+                                   colour = "black"),
+          panel.grid = element_line(colour = "gray70", size = 0.2)) +
+    annotation_custom(tableGrob(round(chi_tb, 3), theme = mytheme.b, rows = NULL),
+                      xmin=((unique(ic$N)*2)-(unique(ic$N)*2)*0.4), xmax=((unique(ic$N)*2)-(unique(ic$N)*2)*0.2),
+                      ymin = max(class_contrib)-(max(class_contrib)*0.6), ymax=max(class_contrib)) +
+    geom_point(aes(x = N, y = CC))
+} else {
+  mx <- matrix(nrow = length(iid), ncol = 16)
+  colnames(mx) <- c("Population", "SFS", "Chi-squared", "p-value", paste("observed", tv[1], tv[2], sep = '_'),
+                    paste("observed", tv[3], tv[4], sep = '_'), paste("sum", tv[1], tv[2], sep = '_'),
+                    paste("sum", tv[3], tv[4], sep = '_'), paste("expected", tv[1], tv[2], sep = '_'),
+                    paste("expected", tv[3], tv[4], sep = '_'), paste("sum_expected", tv[1], tv[2], sep = '_'),
+                    paste("sum_expected", tv[3], tv[4], sep = '_'), paste("residuals_obs", tv[1], tv[2], sep = '_'),
+                    paste("residuals_obs", tv[3], tv[4], sep = '_'), paste("residuals_sum", tv[1], tv[2], sep = '_'),
+                    paste("residuals_sum", tv[3], tv[4], sep = '_'))
+  for (i in 1:length(iid)) {
+    # i <- 41
+    tt <- data.frame(c=c(iid[i],jsd[i]), t=c(sum(iid[-i]),sum(jsd[-i])))
+    cs <- chisq.test(tt)
+    mx[i,1] <- parts
+    mx[i,2] <- paste(tv, collapse = '_')
+    mx[i,3] <- round(cs$statistic, 4)
+    mx[i,4] <- round(cs$p.value, 4)
+    mx[i,5] <- iid[i]
+    mx[i,6] <- jsd[i]
+    mx[i,7] <- sum(iid[-i])
+    mx[i,8] <- sum(jsd[-i])
+    mx[i,9] <- round(cs$expected[1,1])
+    mx[i,10] <- round(cs$expected[2,1])
+    mx[i,11] <- round(cs$expected[1,2])
+    mx[i,12] <- round(cs$expected[2,2])
+    mx[i,13] <- round(cs$residuals[1,1], 4)
+    mx[i,14] <- round(cs$residuals[2,1], 4)
+    mx[i,15] <- round(cs$residuals[1,2], 4)
+    mx[i,16] <- round(cs$residuals[2,2], 4)
+  }
+  mx <- as.data.frame(mx)
+  mx$`p-value` <- as.numeric(as.character(mx$`p-value`))
+  mx$`Chi-squared` <- as.numeric(as.character(mx$`Chi-squared`))
+  mx$signif <- ifelse(test = mx$`p-value` < 0.05, yes = TRUE, no = FALSE)
+  mx$pal <- ifelse(test = mx$signif == TRUE, yes = "red", no = "black")
+  mx$N <- 1:nrow(mx)
+  # table(mx$signif)
+  # mx[mx$signif==TRUE,]
+  
+  write.table(x = mx, file = paste('results/SFS_comp/SFS', parts, paste(tv, collapse = '_'),
+                                   'chi_noinv.csv', sep = "_"), append = FALSE, quote = FALSE, sep = ",",
+              row.names = FALSE, col.names = TRUE)
+  
+  contr_p <- ggplot(data = mx) +
+    geom_point(aes(x = N, y = `Chi-squared`), col = mx$pal) +
+    labs(x = 'Derived allele class', y = 'Contribution') +
+    theme(axis.text = element_text(size = 11),
+          axis.title = element_text(size = 14),
+          panel.background = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+          axis.line = element_line(size = 0.2, linetype = "solid",
+                                   colour = "black"),
+          panel.grid = element_line(colour = "gray70", size = 0.2))
+}
 
-LL0 <- sum(iid*log(joint_p)) + sum(jsd*log(joint_p))
 
-LL1 <- sum(iid*log(iid_p)) + sum(jsd*log(jsd_p))
-
-a <- rep(0,(unique(sc$N)*2)-1)
-for (i in 1:((unique(sc$N)*2)-1)){a[i] <- iid[i]*log(iid_p[i]) + jsd[i]*log(jsd_p[i]) + sum(iid[-i])*log(sum(iid_p[-i])) + sum(jsd[-i])*log(sum(jsd_p[-i]))}
-b <- rep(0,(unique(sc$N)*2)-1)
-for (i in 1:((unique(sc$N)*2)-1)){b[i] <- (iid[i]*log(joint_p[i])) + (jsd[i]*log(joint_p[i])) + sum(iid[-i])*log(sum(joint_p[-i])) + sum(jsd[-i])*log(sum(joint_p[-i]))}
-
-# class_contrib <- rep(0,((unique(sc$N)*2)-1))
-for (i in 1:((unique(sc$N)*2)-1)){class_contrib[i] <- 2*(a[i]-b[i])}
-
-chi_app <- 2*(LL1-LL0)  # now with 16 df (I think)
-cat('Chi-square test statistic =', chi_app, '\n')
-# qchisq(p = 0.05, df = 260)
-ndf <- ((unique(sc$N)*2)-1-1)*(length(unique(da$AV))-1)
-cat('Chi-square critical value at 0.01 with', ndf, 'df =', qchisq(p = 0.01, df = ndf), '\n')
-cat('Test statistic - critical value =', chi_app-qchisq(p = 0.01, df = ndf), '\n')
-
-chi_tb <- data.frame(chi2_stat=chi_app,
-                     chi2_crit=qchisq(p = 0.01, df = ndf),
-                     diff=chi_app-qchisq(p = 0.01, df = ndf))
-# chi_tb
-colnames(chi_tb) <- c("x2 stat", "x2 crit", "x2 stat-crit")
 
 # plot(1:((unique(sc$N)*2)-1), class_contrib)
 
-mytheme.b <- gridExtra::ttheme_default(
-  core = list(fg_params=list(cex = 0.7)),
-  colhead = list(fg_params=list(cex = 0.7, col = 'black')),
-  rowhead = list(fg_params=list(cex = 0.7)))
-contr_p <- ggplot(data = data.frame(N=1:((unique(ic$N)*2)-1), CC=class_contrib)) +
-  labs(x = 'Derived allele class', y = 'Contribution') +
-  theme(axis.text = element_text(size = 11),
-        axis.title = element_text(size = 14),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.5),
-        axis.line = element_line(size = 0.2, linetype = "solid",
-                                 colour = "black"),
-        panel.grid = element_line(colour = "gray70", size = 0.2)) +
-  annotation_custom(tableGrob(round(chi_tb, 3), theme = mytheme.b, rows = NULL),
-                    xmin=((unique(ic$N)*2)-(unique(ic$N)*2)*0.4), xmax=((unique(ic$N)*2)-(unique(ic$N)*2)*0.2),
-                    ymin = max(class_contrib)-(max(class_contrib)*0.6), ymax=max(class_contrib)) +
-  geom_point(aes(x = N, y = CC))
 
 
 da_cc <- DAC_h / dDAP_h / contr_p +
@@ -166,13 +245,14 @@ da_cc <- DAC_h / dDAP_h / contr_p +
 # parts <- strsplit(file_path_sans_ext(basename('summary/allele_count/AC_CZA_CRAB_INDEL_filt2_66N.csv')), split = "_")[[1]]
 # parts <- strsplit(file_path_sans_ext(basename('summary/allele_count/AC_CZA_CRAB_INDEL_filt2_59N.csv')), split = "_")[[1]]
 # parts <- strsplit(file_path_sans_ext(basename(opt$vone)), split = "_")[[1]]
-parts <- cnm[c(-1, (-nrow(cnm)+2):-nrow(cnm)), ]
-if (identical(as.character(parts[,1]),as.character(parts[,2]))) {
-  parts <- paste(as.character(parts[,1]), collapse = '_')
+
+if (opt$test1) {
+  ggsave(filename = paste('figures/SFS', parts, paste(tv, collapse = '_'),
+                          'chi_noinv.pdf', sep = "_"), plot = da_cc, scale = 0.75, dpi = "screen")
 } else {
-  parts <- paste(paste(as.character(parts[,1]), collapse = '_'), paste(as.character(parts[,2]), collapse = '_'), sep = '_')
+  ggsave(filename = paste('figures/SFS_comp/SFS', parts, paste(tv, collapse = '_'),
+                          'chi_noinv.pdf', sep = "_"), plot = da_cc, scale = 0.75, dpi = "screen")
 }
-ggsave(filename = paste('figures/SFS', parts, paste(tv, collapse = '_'),
-                        'chi_noinv.pdf', sep = "_"), plot = da_cc, scale = 0.75, dpi = "screen")
+
 
 
